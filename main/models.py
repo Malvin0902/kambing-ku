8012b99cf01abc063254a757772f95248c406ee0
Malvin <malvinmraqin@gmail.com>
2024-09-17 22:28:46 +0700

    following tutorial 2

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -9,3 +10,10 @@ class Product(models.Model):
     @property
     def is_kambing_pricy(self):
         return self.price > 20000000
+    
+class MoodEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
+    mood = models.CharField(max_length=255)
+    time = models.DateField(auto_now_add=True)
+    feelings = models.TextField()
+    mood_intensity = models.IntegerField()
